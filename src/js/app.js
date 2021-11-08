6987663930ce37dc037c68221b8ade9a53206a43
Andrew Alva <andrew@activetheory.net>
2021-11-07 22:14:55 -0600

    Connect MIDI with GUI. Structure code to load MIDI before initializing 3D scenes. Wrapped MIDI controls in a class; wrapped scene1 code to prevent initializing itself.

--- src/js/app.js
+++ src/js/app.js
@@ -1,105 +1,126 @@
 import '../css/app.css'
+import './math.js'
 import './utils.js'
 import './events.js'
 
-import './midi.js'
+import { MIDI } from './midi.js'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-import { scene1 } from './scene1.js';
+import { Scene1 } from './scene1.js';
 
-/**
- * GUI
- */
-var globalDebugger = window.Utils.gui.addFolder('Global');
-globalDebugger.open();
+var App = {
+    init: function() {
+        var _this = this;
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+        var midiControls = new MIDI();
+        midiControls.init(_this.start);
+    },
 
-// Scene Manager
-var scene = scene1;
+    start: function() {
+        Scene1.init();
+
+        /**
+         * GUI
+         */
+        var globalDebugger = Utils.gui.addFolder('Global');
+        globalDebugger.open();
+
+        // Canvas
+        const canvas = document.querySelector('canvas.webgl')
+
+        // Scene Manager
+        var scene = Scene1.scene;
 
 
-window.addEventListener('resize', () =>
-{
-    // Update camera
-    camera.aspect = Utils.sizes.width / Utils.sizes.height
-    camera.updateProjectionMatrix()
+        window.addEventListener('resize', () =>
+        {
+            // Update camera
+            camera.aspect = Utils.sizes.width / Utils.sizes.height
+            camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(Utils.sizes.width, Utils.sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+            // Update renderer
+            renderer.setSize(Utils.sizes.width, Utils.sizes.height)
+            renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+        })
 
 
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, Utils.sizes.width / Utils.sizes.height)
-camera.position.z = 3
-// scene.add(camera) // Apparently this isn't needed
+        /**
+         * Camera
+         */
+        const camera = new THREE.PerspectiveCamera(75, Utils.sizes.width / Utils.sizes.height)
+        camera.position.z = 3
+        // scene.add(camera) // Apparently this isn't needed
 
-const frontCamera = {};
-frontCamera.quaternion = new THREE.Quaternion().copy(camera.quaternion);
-frontCamera.pos = new THREE.Vector3().copy(camera.position);
-camera.currentPosition = 'frontCamera';
-
-const highAngleCamera = {};
-highAngleCamera.quaternion = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3(0.3608, -0.1314, 0.8675), 0.3161);
-highAngleCamera.pos = new THREE.Vector3(1.6286, 1.9301, -1.6192);
-
-window.Utils.debugger.toggleCamera = function() {
-    if (camera.currentPosition == 'frontCamera') {
-        camera.quaternion.copy(highAngleCamera.quaternion);
-        camera.position.copy(highAngleCamera.pos);
-        camera.currentPosition = 'highAngleCamera';
-    } else {
-        camera.quaternion.copy(frontCamera.quaternion);
-        camera.position.copy(frontCamera.pos);
+        const frontCamera = {};
+        frontCamera.quaternion = new THREE.Quaternion().copy(camera.quaternion);
+        frontCamera.pos = new THREE.Vector3().copy(camera.position);
         camera.currentPosition = 'frontCamera';
+
+        const highAngleCamera = {};
+        highAngleCamera.quaternion = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3(0.3608, -0.1314, 0.8675), 0.3161);
+        highAngleCamera.pos = new THREE.Vector3(1.6286, 1.9301, -1.6192);
+
+        Utils.debugger.toggleCamera = function() {
+            if (camera.currentPosition == 'frontCamera') {
+                camera.quaternion.copy(highAngleCamera.quaternion);
+                camera.position.copy(highAngleCamera.pos);
+                camera.currentPosition = 'highAngleCamera';
+            } else {
+                camera.quaternion.copy(frontCamera.quaternion);
+                camera.position.copy(frontCamera.pos);
+                camera.currentPosition = 'frontCamera';
+            }
+        }
+
+        globalDebugger.add(Utils.debugger, 'toggleCamera');
+        midiEvents.addEventListener('P1_push', Utils.debugger.toggleCamera)
+
+        // Controls
+        const controls = new OrbitControls(camera, canvas)
+        controls.enableDamping = true
+
+
+        /**
+         * Renderer
+         */
+        const renderer = new THREE.WebGLRenderer({
+            canvas: canvas
+        })
+        renderer.setSize(Utils.sizes.width, Utils.sizes.height)
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+        /**
+         * Animate
+         */
+        const clock = new THREE.Clock();
+
+        const tick = () =>
+        {
+        // Utils update
+        Utils.elapsedTime = clock.getElapsedTime();
+            
+        // Update controls
+        controls.update()
+
+        //  Scene updatee
+        scene.update();
+            
+        // Render
+        renderer.render(scene, camera)
+
+        // Call tick again on the next frame
+        window.requestAnimationFrame(tick)
+        }
+
+        tick()
     }
 }
 
-globalDebugger.add(window.Utils.debugger, 'toggleCamera');
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
 
 
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(Utils.sizes.width, Utils.sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-
-/**
- * Animate
- */
-const clock = new THREE.Clock();
-
-const tick = () =>
-{
-// Utils update
-Utils.elapsedTime = clock.getElapsedTime();
-    
-// Update controls
-controls.update()
-
-//  Scene updatee
-scene.update();
-    
-// Render
-renderer.render(scene, camera)
-
-// Call tick again on the next frame
-window.requestAnimationFrame(tick)
+window.onload = function() {
+    App.init();
 }
-
-tick()
  
--- src/js/scene1.js
+++ src/js/scene1.js
@@ -2,64 +2,90 @@ import * as THREE from 'three'
 import testVertexShader from './shaders/test/vertex.glsl'
 import testFragmentShader from './shaders/test/fragment.glsl'
 
-
-/**
- * GUI
- */
-var scene1debugger = window.Utils.gui.addFolder('Scene 1');
-scene1debugger.open();
-
-
-/**
- * Scene
- */
-const scene1 = new THREE.Scene()
-
-
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const geometry2 = new THREE.BoxGeometry(5, 5, 5)
-const material = new THREE.ShaderMaterial({
-    vertexShader: testVertexShader,
-    fragmentShader: testFragmentShader,
-    side: THREE.DoubleSide,
-    transparent: true,
-    depthTest: false,
-    uniforms: {
-        uRadius: { value: 0.45 },
-        uAlpha: { value: 0.8 },
+var Scene1 = {
+    init: function() {
+        var _this = this;
+        _this.start();
     },
-});
 
-scene1debugger.add(material.uniforms.uRadius, 'value').min(0.0001).max(0.45).step(0.0001).name('uRadius');
+    start: function() {
+        var _this = this;
 
-
-const mesh = new THREE.Mesh(geometry, material)
-const mesh2 = new THREE.Mesh(geometry2, material)
-scene1.add(mesh)
-scene1.add(mesh2)
-
-
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, Utils.sizes.width / Utils.sizes.height)
-camera.position.z = 9
-scene1.add(camera)
-scene1.myCamera = camera;
+        /**
+         * GUI
+         */
+        const scene1debugger = window.Utils.gui.addFolder('Scene 1');
+        scene1debugger.open();
+        const scene1Controller = {};
 
 
-/**
- * Animations
- */
-scene1.update = function() {
-    let time = Utils.elapsedTime * 1;
-    let alpha = Math.abs( Math.sin(time) );
+        /**
+         * Scene
+         */
+        _this.scene = new THREE.Scene()
 
-    material.uniforms.uAlpha.value = alpha;
+
+        /**
+         * Object
+         */
+        const geometry = new THREE.BoxGeometry(1, 1, 1)
+        const geometry2 = new THREE.BoxGeometry(5, 5, 5)
+        const material = new THREE.ShaderMaterial({
+            vertexShader: testVertexShader,
+            fragmentShader: testFragmentShader,
+            side: THREE.DoubleSide,
+            transparent: true,
+            depthTest: false,
+            uniforms: {
+                uRadius: { value: 0.45 },
+                uAlpha: { value: 0.8 },
+            },
+        });
+
+        scene1Controller.uRadius = scene1debugger.add(material.uniforms.uRadius, 'value').min(0.0001).max(0.45).step(0.0001).name('uRadius');
+        midiEvents.addEventListener('K1_change', updateRadius);
+
+
+        const mesh = new THREE.Mesh(geometry, material)
+        const mesh2 = new THREE.Mesh(geometry2, material)
+        _this.scene.add(mesh)
+        _this.scene.add(mesh2)
+
+
+        /**
+         * Camera
+         */
+        const camera = new THREE.PerspectiveCamera(75, Utils.sizes.width / Utils.sizes.height)
+        camera.position.z = 9
+        _this.scene.add(camera)
+        _this.scene.myCamera = camera;
+
+
+        /**
+         * Animations
+         */
+        _this.scene.update = function() {
+            let time = Utils.elapsedTime * 1;
+            let alpha = Math.abs( Math.sin(time) );
+
+            material.uniforms.uAlpha.value = alpha;
+        }
+
+
+        /**
+         * MIDI Handlers
+         */
+        function updateRadius(e) {
+            let val = Math.range(e.velocity, 0, 127, 0.0001, 0.45);
+            scene1Controller.uRadius.object.value = val;
+            scene1Controller.uRadius.updateDisplay();
+
+        }
+    }
 }
 
 
-export {scene1};
+
+
+
+export {Scene1};
