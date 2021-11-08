c3638e3353576607bcbd319f216a58ee4ec74dd9
Andrew Alva <andrew@activetheory.net>
2021-11-07 21:59:33 -0600

    Added Math utils

---
+++ src/js/math.js
@@ -0,0 +1,5 @@
+Math.map = Math.range = function(value, oldMin = -1, oldMax = 1, newMin = 0, newMax = 1, isClamp) {
+    const newValue = (((value - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin;
+    if (isClamp) return Math.clamp(newValue, Math.min(newMin, newMax), Math.max(newMin, newMax));
+    return newValue;
+};
